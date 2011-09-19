12bf429d1e4e384f434c24b68af61a88c1254653
Jay Roberts <jay@gloryfish.org>
2011-09-19 19:31:55 -0400

    Initial Commit

---
+++ Animator/AnimatorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AnimatorAppDelegate.h
+//  Animator
+//
+//  Created by Jay Roberts on 9/19/11.
+//  Copyright 2011 GloryFish.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AnimatorAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Animator/AnimatorAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AnimatorAppDelegate.m
+//  Animator
+//
+//  Created by Jay Roberts on 9/19/11.
+//  Copyright 2011 GloryFish.org. All rights reserved.
+//
+
+#import "AnimatorAppDelegate.h"
+
+@implementation AnimatorAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
